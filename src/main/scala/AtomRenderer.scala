accedc7798a21944d386027cf85131103431b4e3
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-07-27 10:16:14 +0100

    Support for defaultHtml

--- src/main/scala/AtomRenderer.scala
+++ src/main/scala/AtomRenderer.scala
@@ -18,23 +18,25 @@ trait AtomRenderer {
 
   def getJS[A <: AtomData](atom: Atom)(implicit r: Resource[A]): JS =
     r.js(atom).map(_.toString)
+
+  def getDefaultHTML(atom: Atom): HTML = atom.defaultHtml
 }
 
 object AtomRenderer extends AtomRenderer {
   def apply(atom: Atom) = atom.atomType match {
-    case AtomType.Cta            => Some(getAll[AtomData.Cta]           (atom))
-    case AtomType.Explainer      => Some(getAll[AtomData.Explainer]     (atom))
-    case AtomType.Guide          => Some(getAll[AtomData.Guide]         (atom))
-    case AtomType.Interactive    => Some(getAll[AtomData.Interactive]   (atom))
-    case AtomType.Media          => Some(getAll[AtomData.Media]         (atom))
-    case AtomType.Profile        => Some(getAll[AtomData.Profile]       (atom))
-    case AtomType.Qanda          => Some(getAll[AtomData.Qanda]         (atom))
-    case AtomType.Quiz           => Some(getAll[AtomData.Quiz]          (atom))
-    case AtomType.Recipe         => Some(getAll[AtomData.Recipe]        (atom))
-    case AtomType.Review         => Some(getAll[AtomData.Review]        (atom))
-    case AtomType.Storyquestions => Some(getAll[AtomData.Storyquestions](atom))
-    case AtomType.Timeline       => Some(getAll[AtomData.Timeline]      (atom))
-    case _ => None
+    case AtomType.Cta            => getAll[AtomData.Cta]           (atom)
+    case AtomType.Explainer      => getAll[AtomData.Explainer]     (atom)
+    case AtomType.Guide          => getAll[AtomData.Guide]         (atom)
+    case AtomType.Interactive    => getAll[AtomData.Interactive]   (atom)
+    case AtomType.Media          => getAll[AtomData.Media]         (atom)
+    case AtomType.Profile        => getAll[AtomData.Profile]       (atom)
+    case AtomType.Qanda          => getAll[AtomData.Qanda]         (atom)
+    case AtomType.Quiz           => getAll[AtomData.Quiz]          (atom)
+    case AtomType.Recipe         => getAll[AtomData.Recipe]        (atom)
+    case AtomType.Review         => getAll[AtomData.Review]        (atom)
+    case AtomType.Storyquestions => getAll[AtomData.Storyquestions](atom)
+    case AtomType.Timeline       => getAll[AtomData.Timeline]      (atom)
+    case _                       => (getDefaultHTML(atom), None, None)
   }
 }
 
