bb8fef3d043e641a41cd4953615d076a63eefca0
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-08-23 12:01:34 +0100

    Use getHTML by default

--- src/main/scala/AtomRenderer.scala
+++ src/main/scala/AtomRenderer.scala
@@ -12,40 +12,37 @@ trait AtomRenderer {
   type CSS = Option[String]
   type JS = Option[String]
 
-  def apply: Atom => (HTML, CSS, JS) = getAll
+  def apply: Atom => HTML = getHTML
 
   def getHTML[A](atom: Atom, data: A)(implicit reader: Rendering[A]): HTML =
     reader.html(atom, data).toString
+  
+  def getHTML(atom: Atom): HTML = atom.data match {
+    case AtomData.Cta(data)            => getHTML(atom, data)
+    case AtomData.Explainer(data)      => getHTML(atom, data)
+    case AtomData.Guide(data)          => getHTML(atom, data)
+    case AtomData.Interactive(data)    => getHTML(atom, data)
+    case AtomData.Media(data)          => getHTML(atom, data)
+    case AtomData.Profile (data)       => getHTML(atom, data)
+    case AtomData.Qanda(data)          => getHTML(atom, data)
+    case AtomData.Quiz(data)           => getHTML(atom, data)
+    case AtomData.Recipe(data)         => getHTML(atom, data)
+    case AtomData.Review(data)         => getHTML(atom, data)
+    case AtomData.Storyquestions(data) => getHTML(atom, data)
+    case AtomData.Timeline(data)       => getHTML(atom, data)
+    case _                             => atom.defaultHtml
+  }
+
+  def getHTML(json: Json): Option[HTML] = json.as[Atom] match {
+    case Left(_) => None
+    case Right(atom) => Some(getHTML(atom))
+  } 
 
   def getCSS[A](implicit reader: Rendering[A]): CSS =
     reader.css.map(_.toString)
 
   def getJS[A](implicit reader: Rendering[A]): JS =
     reader.js.map(_.toString)
-
-  def getAll[A : Rendering](atom: Atom, data: A): (HTML, CSS, JS) =
-    (getHTML(atom, data), getCSS[A], getJS[A])
-  
-  def getAll(atom: Atom): (HTML, CSS, JS) = atom.data match {
-    case AtomData.Cta(data)            => getAll(atom, data)
-    case AtomData.Explainer(data)      => getAll(atom, data)
-    case AtomData.Guide(data)          => getAll(atom, data)
-    case AtomData.Interactive(data)    => getAll(atom, data)
-    case AtomData.Media(data)          => getAll(atom, data)
-    case AtomData.Profile (data)       => getAll(atom, data)
-    case AtomData.Qanda(data)          => getAll(atom, data)
-    case AtomData.Quiz(data)           => getAll(atom, data)
-    case AtomData.Recipe(data)         => getAll(atom, data)
-    case AtomData.Review(data)         => getAll(atom, data)
-    case AtomData.Storyquestions(data) => getAll(atom, data)
-    case AtomData.Timeline(data)       => getAll(atom, data)
-    case _                             => (atom.defaultHtml, None, None)
-  }
-
-  def getAll(json: Json): Option[(HTML, CSS, JS)] = json.as[Atom] match {
-    case Left(_) => None
-    case Right(atom) => Some(getAll(atom))
-  } 
 }
 
 object ArticleAtomRenderer extends AtomRenderer {
