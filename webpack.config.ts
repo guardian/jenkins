f8dcf9778f3579646ab866d73bae330e1ad80aa6
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-09-12 10:52:19 +0100

    Add webpack

---
+++ webpack.config.ts
@@ -0,0 +1,57 @@
+const path = require('path');
+
+const babelOptions = {
+  "presets": [
+    [
+      "es2015",
+      {
+        "modules": false
+      }
+    ],
+    "es2016",
+    "es2017"
+  ]
+};
+
+module.exports = {  
+  entry: {
+    guide: './guide/article/index.js',
+    qanda: './qanda/article/index.js',
+    profile: './profile/article/index.js',
+    timeline: './timeline/article/index.js',
+  },
+  output: {
+    filename: '[resource-path]/[name].transpiled.js',
+    path: path.resolve(__dirname, 'src', 'main', 'resources'),
+    libraryTarget: 'var'
+  },
+  resolve: {
+      // Add '.ts' and '.tsx' as a resolvable extension.
+      extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: babelOptions
+        },
+        {
+          loader: 'ts-loader'
+        }
+      ]
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: babelOptions
+        }
+      ]
+    }]
+  },
+  context: path.resolve(__dirname, 'src', 'main', 'resources')
+};
