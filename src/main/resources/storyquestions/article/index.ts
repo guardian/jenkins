25dd22f0968edce8340e76f36d5947cbd1f8b8d2
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-09-20 10:55:19 +0100

    Empty storyquestions bundle

--- src/main/resources/storyquestions/article/index.ts
+++ src/main/resources/storyquestions/article/index.ts
@@ -1,19 +1,1 @@
-interface OphanRecord {
-  [propName: string]: number | string | boolean
-};
 
-type OphanService = {
-  record: (x: OphanRecord) => never
-};
-
-type Services = {
-  ophan: OphanService
-};
-
-export default function({ ophan }: Services) {
-  const secretNumber = Math.random() * 123456789
-
-  ophan.record({
-    secretNumber: secretNumber
-  });
-}
