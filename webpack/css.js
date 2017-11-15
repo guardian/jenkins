bb37d54b14b67d32a01728cf90d014739758396a
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-15 12:33:18 +0000

    Handle CSS vars

---
+++ webpack/css.js
@@ -0,0 +1,24 @@
+const app = '@guardian/atom-renderer';
+export default (varFile) => {
+  const variables = require(`babel-loader!postcss-variables-loader!sass-loader!$varFile`)
+
+  return [
+    {
+        test: /@guardian\/atom-renderer\/dist\/.+\.css$/,
+        use: [
+            'style-loader', 
+            'css-loader',
+            { 
+              loader: 'postcss-loader', 
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  require('autoprefixer')(),
+                  require('postcss-css-variables')({ variables })
+                ]
+              }
+            }
+        ]
+    },
+  ]
+};
