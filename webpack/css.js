92ee8d5cf2b9f5d6cef71405001a306331054d53
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-15 15:44:20 +0000

    Don't use vars in JS anymore

--- webpack/css.js
+++ webpack/css.js
@@ -1,29 +1,34 @@
-module.exports = (variables) => {
-  return [
-    {
-      test: /stylesheets\/atoms\.scss$/,
-      use: [
-        'babel-loader'
-        'postcss-variables-loader', 
-        'sass-loader',
-      ]
-    },
-    {
-        test: /@guardian\/atom-renderer\/dist\/.+\.css$/,
-        use: [
-            'style-loader', 
-            'css-loader',
-            { 
-              loader: 'postcss-loader', 
-              options: {
-                ident: 'postcss',
-                plugins: () => [
-                  require('autoprefixer')(),
-                  require('postcss-css-variables')({ variables })
-                ]
-              }
-            }
-        ]
-    },
-  ]
-};
+module.exports = (cssVarsPath) => [
+  {
+    test: /stylesheets\/atoms\.scss$/,
+    use: [
+      'babel-loader'
+      'postcss-variables-loader', 
+      'sass-loader',
+    ]
+  },
+  {
+    test: /@guardian\/atom-renderer\/dist\/.+\.css$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: () => [
+            require('postcss-import')(),
+            require('autoprefixer')(),
+            require('postcss-css-variables')()
+          ]
+        }
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          data: `@import '${cssVarsPath}';`
+        }
+      },
+    ]
+  }
+];
