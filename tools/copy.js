85c5a1e6806c4b0a54892cc9a97d03f9c92a25a9
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-16 15:01:39 +0000

    Remove catch handler

--- tools/copy.js
+++ tools/copy.js
@@ -3,4 +3,4 @@ const copy = require('recursive-copy');
 copy('src/main/resources/__flow__', 'dist/__flow__', {
   overwrite: true,
   rename: filepath => filepath.replace('.fjs', '.js')
-}).catch(() => {});
+});
