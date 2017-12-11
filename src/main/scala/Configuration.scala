15f07c75669292478095a2b1ec3b9b3460279ef8
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-12-11 17:17:22 +0000

    Add ArticleConfiguration

--- src/main/scala/Configuration.scala
+++ src/main/scala/Configuration.scala
@@ -4,3 +4,9 @@ sealed trait Configuration
 
 sealed trait NilConfiguration extends Configuration
 case object NilConfiguration extends NilConfiguration
+
+sealed trait ArticleConfiguration extends Configuration {
+  def ajaxUrl: String
+}
+
+final case class SimpleArticleConfiguration(ajaxUrl: String) extends ArticleConfiguration
