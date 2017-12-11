45358ff3380f0622b36d95428baa6224eb805a5a
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-12-11 17:08:21 +0000

    Add configuration to Rendering

--- src/main/scala/renderers/Rendering.scala
+++ src/main/scala/renderers/Rendering.scala
@@ -6,7 +6,13 @@ import com.gu.contentatom.thrift.Atom
 import play.twirl.api.Html
 
 trait Rendering[A] {
-  def html(atom: Atom, data: A): Html
+  type Conf <: Configuration
+
+  def html(atom: Atom, data: A)(implicit conf: Conf): Html
   def css: Option[String]
   def js: Option[String]
+}
+
+object Rendering {
+  type Aux[A, C] = Rendering[A] { type Conf = C }
 }
