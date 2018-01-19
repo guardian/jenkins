be6786effc41a99432117812cb7fe014caf33dc4
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-01-19 15:10:45 +0000

    Update email header

--- src/main/scala/Configuration.scala
+++ src/main/scala/Configuration.scala
@@ -9,18 +9,18 @@ case object NilConfiguration extends NilConfiguration
 
 sealed trait EmailConfiguration extends Configuration {
   def campaignServiceUrl: String
-  def guardianLogoUrl: String
-  def productLogoUrl: String
-  def viewInBrowserUrl: Atom => String
+  def siteUrl: String
+  def logoUrl: String
   def userProfileUrl: String
+  def viewInBrowserUrl: Atom => String
 }
 
 final case class SimpleEmailConfiguration(
   campaignServiceUrl: String,
-  guardianLogoUrl: String,
-  productLogoUrl: String,
+  siteUrl: String,
+  logoUrl: String,
   userProfileUrl: String
 ) extends EmailConfiguration {
-  val viewInBrowserUrl = atom =>
+  def viewInBrowserUrl = atom =>
     s"${campaignServiceUrl}/preview/${atom.id}"
 }
