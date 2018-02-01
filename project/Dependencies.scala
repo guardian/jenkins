501f206e1bb5cbcbf58f16173b7da60f4fcb8323
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-02-01 16:40:17 +0000

    Bump CAPI versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,13 +4,13 @@ object Dependencies {
   lazy val circeVersion = "0.8.0"
 
   val coreDeps = Seq(
-    "com.gu"   %% "content-atom-model" % "2.4.55",
+    "com.gu"   %% "content-atom-model" % "2.4.61",
     "org.jsoup" % "jsoup"              % "1.11.2",
     "org.scalacheck" %% "scalacheck"   % "1.13.5" % "test"
   )
 
   val utilsDeps = Seq(
-    "com.gu"   %% "content-api-client" % "11.49",
+    "com.gu"   %% "content-api-client" % "11.51",
     "org.typelevel" %% "cats-core"     % "1.0.1",
     "io.monix" %% "monix"              % "3.0.0-M3"
   )
